639cf18ca5027593995dc15e1d17abad9b2cf778
kuomat <kuomatthew20040529@gmail.com>
2023-06-04 00:40:49 +0800

    Included gpu usage

--- model/preprocessing.py
+++ model/preprocessing.py
@@ -2,10 +2,14 @@
 from sentence_transformers import SentenceTransformer
 import pandas as pd
 import numpy as np
+import time
+
 #%%
 
+device = "cuda:4"
+
 #https://huggingface.co/sentence-transformers/paraphrase-MiniLM-L6-v2
-model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+model = SentenceTransformer('paraphrase-MiniLM-L6-v2').to(device)
 
 #%%
 
@@ -13,11 +17,12 @@ headlines = pd.read_csv('../news.csv')
 
 
 #%%
-
-embeddings = model.encode(headlines["Headlines"]) # type: ignore
+start_time = time.time()
+embeddings = model.encode(headlines["Headlines"], convert_to_tensor=True, device=device)
+print(f"Model took {time.time() - start_time} to encode")
 
 #%%
-newData = pd.DataFrame(embeddings) # type: ignore
+newData = pd.DataFrame(embeddings.cpu().numpy())
 
 newData.rename(columns=lambda x: f"embedding_{x}", inplace=True)
 
